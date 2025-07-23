7bcbad9e8c5a151af2231bcb2958ded5de7bcd12
hainkiwanki <1174451+hainkiwanki@users.noreply.github.com>
2025-07-23 17:38:34 +0200

    Updated for template: style fix

--- src/plugins/vuetify.mts
+++ src/plugins/vuetify.mts
@@ -1,8 +1,10 @@
-import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import { aliases, mdi } from 'vuetify/iconsets/mdi';
 
 export const vuetify = createVuetify({
+    theme: {
+        defaultTheme: 'dark',
+    },
     icons: {
         defaultSet: 'mdi',
         aliases,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,8 @@
     <v-app>
         <v-main>
             <v-container>
-                <h1>Welcome to My Tool</h1>
+                <h1 class="text-h4 mb-4">Welcome to Your Tool</h1>
+                <p>Add your custom content here</p>
             </v-container>
         </v-main>
     </v-app>
